ebe6620a0ba4b75678f0b88eca8a606a4833d865
Kirillsigma <caxap.mel@mail.ru>
2025-09-08 18:29:29 +0300

    Create project

--- Page/main.py
+++ Page/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Привет, мир! Flask работает!"
 
+@app.route("/")
+def home():
+    return "Привет, мир! Flask работает!"
+
 if __name__ == "__main__":
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-WebFlask
+**WebFlask**
